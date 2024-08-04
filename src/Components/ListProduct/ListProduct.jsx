58e2d4c47ac5ee557ccb338ecbe14a3a9d8dc2f3
Ananddivine <109467220+Ananddivine@users.noreply.github.com>
2024-08-04 18:04:26 +0530

    Initial commit

--- src/Components/ListProduct/ListProduct.jsx
+++ src/Components/ListProduct/ListProduct.jsx
@@ -7,7 +7,7 @@ const ListProduct = () => {
 
   const fetchInfo = async () => {
     try {
-      const response = await fetch('http://localhost:4000/allproducts');
+      const response = await fetch('https://zefefrpdoors-backend.onrender.com/allproducts');
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
@@ -25,7 +25,7 @@ const ListProduct = () => {
 
   const remove_product = async (id) => {
     try {
-      const response = await fetch('http://localhost:4000/removeproduct', {
+      const response = await fetch('https://zefefrpdoors-backend.onrender.com/removeproduct', {
         method: 'POST',
         headers: {
           Accept: 'application/json',
